14418139c684810a114f7ca3bc4e80c00bd023ca
TheProgrammerX <59706713+TheProgrammerX@users.noreply.github.com>
2020-10-04 11:35:28 +0300

    added auth_screen page and make it as start page of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
-import 'package:Shop_App/providers/orders.dart';
-
-import 'package:Shop_App/providers/products_provider.dart';
-import 'package:Shop_App/screens/cart_detail.dart';
-import 'package:Shop_App/screens/edit_product_screen.dart';
-import 'package:Shop_App/screens/orders_screen.dart';
-import 'package:Shop_App/screens/product_detail_screen.dart';
-import 'package:Shop_App/screens/products_overview_screen.dart';
-import 'package:Shop_App/screens/user_products_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:Shop_App/providers/cart.dart';
+
+import './providers/orders.dart';
+
+import './providers/products_provider.dart';
+import './screens/cart_detail.dart';
+import './screens/edit_product_screen.dart';
+import './screens/orders_screen.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/auth_screen.dart';
+
+import './providers/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,7 +39,7 @@ class MyApp extends StatelessWidget {
           fontFamily: "Lato",
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartDetail.routeName: (ctx) => CartDetail(),
